d9702e253ee9f1096ce8726b0e0459752ec49280
c762496760 <762496760@qq.com>
2018-04-25 11:13:49 +0800

    兼容39app

--- src/Pusher/UmengPusher.php
+++ src/Pusher/UmengPusher.php
@@ -6,21 +6,26 @@ class UmengPusher
 {
     private $android = null;
     private $ios = null;
-    public function __construct() {
-        $iosAppKey = config('umeng.ios_app_key');
-        $iosAppMasterSecret = config('umeng.ios_app_master_secret');
-        $androidAppKey = config('umeng.android_app_key');
-        $androidAppMasterSecret = config('umeng.android_app_master_secret');
 
+    public function android($predefined){
+        if($predefined['ticker'] == '76彩票')
+        {
+            $androidAppKey = config('umeng.android_app_key');
+            $androidAppMasterSecret = config('umeng.android_app_master_secret');
+        }
+        elseif($predefined['ticker'] == '39彩票')
+        {
+            $androidAppKey = config('umeng.39_android_app_key');
+            $androidAppMasterSecret = config('umeng.39_android_app_master_secret');
+        }
         $this->android = new AndroidPusher($androidAppKey, $androidAppMasterSecret);
-        $this->ios = new IOSPusher($iosAppKey, $iosAppMasterSecret);
-    }
-
-    public function android(){
         return $this->android;
     }
 
     public function ios(){
+        $iosAppKey = config('umeng.ios_app_key');
+        $iosAppMasterSecret = config('umeng.ios_app_master_secret');
+        $this->ios = new IOSPusher($iosAppKey, $iosAppMasterSecret);
         return $this->ios;
     }
 }
